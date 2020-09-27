e45bb0d0d6275ec968dcde9e8bdfffc59f40e9ff
euclides <euclides.nascimento@vivitech.com>
2020-09-26 22:43:57 -0300

    update store config

--- src/components/Login/Login.vue
+++ src/components/Login/Login.vue
@@ -1,90 +1,53 @@
 <template>
-  <v-card>
-  </v-card>
+  <form>
+    <v-text-field
+      v-model="email"
+      label="E-mail"
+      required
+    ></v-text-field>
+
+    <v-text-field
+      :append-icon="showPass ? 'mdi-eye' : 'mdi-eye-off'"
+      :rules="[rules.required, rules.min]"
+      :type="showPass ? 'text' : 'password'"
+      v-model="password"
+      label="Senha *"
+      hint="At least 8 characters"
+      counter
+      @click:append="showPass = !showPass"
+    ></v-text-field>
+    <v-btn class="mr-4" @click="hendleLogin()">Login</v-btn>
+  </form>
 </template>
 <style scoped>
 </style>
 
 <script>
-import api from "../../config/api";
-import {mapActions, mapGetters} from 'vueex';
+import { mapActions } from "vueex";
 
 export default {
   data: () => ({
-    snackbar: false,
-    search: "",
-    msg: "",
-    showPass: false,
-    password: "",
-    rules: {
-      required: (value) => !!value || "Required.",
-      min: (v) => v.length >= 8 || "Min 8 characters",
-      emailMatch: () => "The email and password you entered don't match",
-    },
-    firsstName: "",
-    secondName: "",
-    name: "",
-    email: "",
-    dialog: false,
-    headers: [
-      { text: "Nome", value: "name" },
-      { text: "E-mail", value: "email" },
-      { text: "Ações", value: "actions", sortable: false },
-    ],
-    provider: [],
+    email: '',
+    password: '',
+    showPass: false
   }),
 
-  computed: {},
+  computed: {
+    error: 'Login/getterError'
+  },
 
   created() {
     this.initialize();
   },
 
   methods: {
-    initialize() {
-      const getUser = async () => {
-        await api
-          .get("/user", {noLimit:true}, {
-          })
-          .then((res) => {
-            console.log("res da api: ", res.data);
-            this.provider = res.data.items;
-          })
-          .catch((error) => {
-            console.log("Error da api: ", error.response.data);
-          });
-      };
-      return getUser();
-    },
+    ...mapActions({
+      login: 'Login/Login',
+    }),
+    initialize() {},
 
-    hendleSubmit() {
-      this.name = this.firsstName + " " + this.secondName;
-      console.log("chamou, e o nome do infeliz é: ");
-
-      const sendUser = async () => {
-        await api
-          .post("/user", {
-            name: this.name,
-            email: this.email,
-            password: this.password,
-          })
-          .then((res) => {
-            console.log("res da api: ", res.data.items);
-            const items = res.data.items;
-            this.email = items.email;
-            this.msg = items.msg;
-            this.dialog = false;
-            this.snackbar = true;
-          })
-          .catch((error) => {
-            console.log("Error da api: ", error.response.data);
-            this.msg = error.response.data;
-            this.dialog = false;
-            this.snackbar = true;
-          });
-      };
-
-      return sendUser();
+    async hendleLogin() {
+      await this.login({email: this.email, password: this.password})
     },
   },
 };
--- src/store/Login/mutations.js
+++ src/store/Login/mutations.js
@@ -2,10 +2,10 @@ export default {
     SET_LOGIN(state, payload) {
         state.login = payload;
     },
-    SET_ERROR(error, payload) {
+    SET_ERROR(state, payload) {
         state.error = payload;
     },
-    CLEAR_ERROR(error) {
+    CLEAR_ERROR(state) {
         state.error = null;
     }
 }
