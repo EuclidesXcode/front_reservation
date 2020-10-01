313e012edaf97548e2b29dff5c9a4c3ec05bd317
euclides <euclides.nascimento@vivitech.com>
2020-10-01 07:38:17 -0300

    update dashboard

---
+++ src/components/Dashboard/Dashboard.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <line-chart :styles="myStyles"/>
+    <button @click="increase()">Increase height</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      height: 300
+    }
+  },
+  methods: {
+    increase () {
+     this.height += 10
+    }
+  },
+  computed: {
+    myStyles () {
+      return {
+        height: `${this.height}px`,
+        position: 'relative'
+      }
+    }
+  }
+}
+</script>
